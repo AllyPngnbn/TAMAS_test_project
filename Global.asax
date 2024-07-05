678ba06055f7c5a187dd9c387f9664a0890be37e
AllyPngnbn <allyssa0926panganiban@gmail.com>
2024-07-06 00:09:17 +0800

    Initial commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TAMAS_test_project.MvcApplication" Language="C#" %>
---
+++ Scripts/LogInJS.js
@@ -0,0 +1,11 @@
+﻿
+$(document).ready(function () {
+    $('#login').click(function () {
+        let un_input = $('#username').val();
+        let pass_input = $('#password').val();
+
+        if ((un_input != '') && (pass_input != '')) {
+            alert('You have successfully logged in!')
+        }
+    });
+})
